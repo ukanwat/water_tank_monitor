d24254d8a9ad2a20d8b89a70184f3484d2fe782c
ukanwat <utkarshkanwat@gmail.com>
2022-02-15 14:27:51 +0530

    5

--- lib/main.dart
+++ lib/main.dart
@@ -1,104 +1,75 @@
 import 'package:flutter/material.dart';
-import 'package:water_tank_monitor/screens/home.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() {
+// providers
+import './providers/home_provider.dart';
+import './providers/auth_provider.dart';
+import './providers/statistics_provider.dart';
+
+// screens
+import './root.dart';
+import './screens/data_entry_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await SystemChrome.setPreferredOrientations(
+    <DeviceOrientation>[
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp
+    ]
+  );
+
+  FlutterLocalNotificationsPlugin notificationsPlugin = FlutterLocalNotificationsPlugin();
+  AndroidInitializationSettings android = AndroidInitializationSettings('notification_icon');
+  IOSInitializationSettings ios = IOSInitializationSettings();
+  InitializationSettings settings = InitializationSettings(
+    android,ios
+  );
+  await notificationsPlugin.initialize(
+    settings,
+  );
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Water Tank Monitor',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Water Monitor'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          actions: [
-            Icon(
-              Icons.settings,
-              color: Colors.black,
-            ),
-            SizedBox(
-              width: 10,
-            )
-          ],
-          leading: Icon(
-            Icons.settings,
-            color: Colors.black,
-          ),
-
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Column(
-            children: [
-              Text(
-                widget.title,
-                style: TextStyle(
-                    color: Colors.black,
-                    letterSpacing: 0.5,
-                    fontFamily: 'TechnaSans',
-                    fontSize: 24),
-              ),
-              Container(
-                height: 4,
-              ),
-              Text(
-                'Tank 1 - SDHSJF88D',
-                style: TextStyle(
-                    color: Colors.white.withOpacity(0.7), fontSize: 16),
-              ),
-            ],
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthProvider>(
+          create: (context) => AuthProvider(),
+        ),
+        ChangeNotifierProxyProvider<AuthProvider,HomeProvider>(
+          create: (context) => HomeProvider(),
+          update: (context, authProvider, homeProvider) => homeProvider..update(authProvider.user),
+        ),
+        ChangeNotifierProxyProvider<AuthProvider,StatisticsProvider>(
+          create: (context) => StatisticsProvider(),
+          update: (context, authProvider, statisticsProvider) => statisticsProvider..update(authProvider.user),
+        )
+      ],
+      child: MaterialApp(
+        //showPerformanceOverlay: true,
+        title: 'Drinkable',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          pageTransitionsTheme: PageTransitionsTheme(
+            builders: {
+              TargetPlatform.android : CupertinoPageTransitionsBuilder()
+            }
           ),
         ),
-        body: Home());
+        home: Root(),
+        routes: {
+          DataEntryScreen.routeName : (ctx)=>DataEntryScreen(),
+        },
+      ),
+    );
   }
 }
